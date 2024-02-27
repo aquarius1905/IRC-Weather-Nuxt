0f42d30f0f0af85e4bb7ed8f3f9cc902469574fd
aquarius1905 <reiko6122@gmail.com>
2024-02-27 21:58:14 +0900

    index.vue実装

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,4 +1,28 @@
 <template>
-  <div>
+  <div class="home flex">
+    <Card v-for="(city, index) in lists" :key="index" :city="city" />
   </div>
 </template>
+
+<script lang="ts" setup>
+const lists = [
+  "sapporo",
+  "tokyo",
+  "nagoya",
+  "osaka",
+  "fukuoka",
+  "naha"
+  ]
+</script>
+
+<style scoped>
+body {
+  background: #eee;
+}
+
+.flex {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
